0f82dea9c4bc0088d383499964351432e033ae06
luk <luk@85-58-28-137.plesk.page>
2020-07-21 16:41:27 +0200

    basic change state react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'Default',
+      time: new Date().toLocaleTimeString(),
+      number: 0,
+      numbers: []
+    }
+  }
+
+  changeState(){
+    let number = Math.round(Math.random() * 4);
+    let numbers = this.state.numbers;
+    numbers.push(number);
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+      numbers: numbers,
+      number: number,
+      title: ((number % 2 === 0) ? 'It is even' : 'It is odd')
+    })
+    console.log('changeState', this.state);
+  }
+  
+  render(){
+    console.log('Render was called', this.state);
+    const colors = ['red', 'yellow', 'green', 'blue', 'orange'];
+    const color = colors[this.state.number];
+    return(
+      <div className="App" style={{backgroundColor: color}}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">
+            {this.state.title} - {this.state.number}
+          </h1>
+        </header>
+        <div className="App-intro">
+          <div>{this.state.time}</div>
+          Press this button to change state
+          <div>
+            <button onClick={() => this.changeState()}>
+              Change state
+            </button>
+          </div>
+          <div>
+            Generated numbers
+            <ul>
+              {this.state.numbers.map((x) => (
+                <li key={x}>{x}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
